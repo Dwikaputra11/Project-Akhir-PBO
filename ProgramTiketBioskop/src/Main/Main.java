dcc5190ef528aae29c5b22a8cfa134c36d1dbcb1
Dwika <dwikariswanda@gmail.com>
2022-05-17 19:06:09 +0700

    commit Dwika 8: add Lihat.class

--- ProgramTiketBioskop/src/Main/Main.java
+++ ProgramTiketBioskop/src/Main/Main.java
@@ -1,19 +1,12 @@
 package Main;
 
-import GUI.PemilihanSeat;
+import GUI.*;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        Login login = new Login();
-        login.initialize();
-        // Tambah tambah = new Tambah();
-        // tambah.initialize();
-=======
         // Login login = new Login();
         // login.initialize();
-        PemilihanSeat seat = new PemilihanSeat();
-        seat.initialize();
->>>>>>> a414b0c59cfa43d7497da4c5e1eab7e85db95bd0
+        Lihat lihat = new Lihat();
+        lihat.initialize();
     }
 }
